fa521cda86c5aeb6cbffeaf16521f88ad4a51caa
Cameron Moreau <cameron.cm6@gmail.com>
2014-12-13 03:13:27 -0600

    First Commit + Login Page
    Initial commit plus the login page is structured and styled. Still
    needs functionality and the navigation at the top.

---
+++ login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Real Estate Website</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="login-wrapper">
+		<div class="login-header">
+			Some title
+		</div>
+		<div class="login-body">
+			<div class="login-title">Login to Your Account</div>
+			<form method="post">
+				<input type="text" placeholder="Email" />
+				<input type="password" placeholder="Password" />
+				<input type="submit" value="Login" class="btn btn-solid1" />
+				<a href="#" class="login-link">Forgot password?</a>
+				<div class="clear"></div>
+			</form>
+			<div class="login-hr">
+				<span>or</span>
+			</div>
+			<a href="#" class="btn btn-holo1">14 Day Trial</a>
+		</div>
+	</div>
+</body>
+</html>
